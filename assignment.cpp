a1d61b56c48813ba51ae8d7d3f426ced8e0ab20c
Quench <clexllama@gmail.com>
2026-06-19 02:45:13 +0800

    start

---
+++ assignment.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Store{
+    private : 
+        int item_code;
+        string category;
+        float price;
+
+
+    public :
+        Store(){
+
+        }
+
+}S1;
+
+class Cart : public Store{
+    private : 
+        int placeholder;
+
+    public :
+
+};
+
+
+class Clothing : public Store{
+    private : 
+        int placeholder;
+
+    public :
+
+};
+
+int main()
+{
+
+
+
+    return 0;
+
+}
